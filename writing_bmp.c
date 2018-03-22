c42612e420c92cdab3e3782627c6ffe161617bbf
Ashish Subedi <ashishsubedi10@gmail.com>
2018-03-22 20:10:20 +0545

    somewhat working writing bmp. Inverses R and B. Dowsn;t work on some 32x32

--- writing_bmp.c
+++ writing_bmp.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+
 #include<string.h>
 
   struct BitMap
@@ -24,10 +26,10 @@
 int main(){
     FILE *fp;
     //CHange image accordingly
-    fp = fopen("./test2.bmp","rb");
+    fp = fopen("./32_2.bmp","rb");
 
     //FOR 2x2,replace w and h
-    int w = 2, h = 2;//Resolution of image. this time 32x32
+    int w = 32, h = 32;//Resolution of image. this time 32x32
     int r = w*h;
     int image[r][3];//3 represents R G B column
 
@@ -47,28 +49,29 @@ int main(){
         
     for(i=0;i<h;i++){
         for(j=0;j<w;j++){
-
+            
             image[(i*h)+j][2] = getc(fp);//BMP SAVES IN BGR Format. So assining in revrese order.[2] = B
             image[(i*h)+j][1] = getc(fp);//[1] = G [0] = R
             image[(i*h)+j][0] = getc(fp);
+
             //BMP IS STORED from bottom left and changing row means going up instead of down
             //So 0,0 indicates bottom left position, 1,1 means top right in 2x2 pixel
-            printf("pixel %d %d : [R:%d,G:%d,B:%d]\n",i,j,image[(i*h)+j][0],image[(i*h)+j][1],image[(i*h)+j][2]);
+    //  printf("pixel %d %d : [R:%d,G:%d,B:%d]\n",i,j,image[(i*h)+j][0],image[(i*h)+j][1],image[(i*h)+j][2]);
            
         }
-        fseek(fp,2,SEEK_CUR);//GETTING TWO bytes ahead because of padding (2 bytes).
+        //fseek(fp,0,SEEK_CUR);//GETTING TWO bytes ahead because of padding (2 bytes).
     }
    
     //JUST TESTING TO CREATE ANOTHER IMAGE WITH DIFFERENT VALUES
         FILE *fq;
-        fq = fopen("code_image.bmp","wb");
+        fq = fopen("code_image_32_2.bmp","wb");
         rewind(fp);//File pointer goes to start
         ///COPIED FROM INTERNET TO ENTER HEADER FILES OF BMP
 
 
  
    
-        memset(&Header, 0, sizeof(Header));
+       // memset(&Header, 0, sizeof(Header));
         
         fread(&Header.Type, 2, 1, fp);
         fread(&Header.Size, 4, 1, fp);
@@ -106,8 +109,15 @@ int main(){
 
         //NOW for changing some parts .. I AM GONNA CHANGE MSB INSTEAD OF LSB .. LETS SEE
 
-        int c = 5;
+       // int c = 5;
+       fseek(fq,54,SEEK_SET);
         int R,G,B;
+        for(i=0;i<h;i++){
+            for(j=0;j<w;j++){
+            }
+        }
+              
+                printf("\n");
         for(i=0;i<h;i++){
             for(j=0;j<w;j++){
 
@@ -119,21 +129,38 @@ int main(){
                 R =  image[(i*h)+j][0];
                 G =  image[(i*h)+j][1];
                 B =  image[(i*h)+j][2];
-/* 
-                R = R - R%2;// Makes LSB 0??? THis works because if odd, 2^0 is 1, if even 2^0 = 0. So 
-                B = B - B%2;//If odd, remainder is 1, and subtracts it. Else does nothing
-                G = G - G%2; */
+
+             /*    R = R - R/3;// Makes LSB 0??? THis works because if odd, 2^0 is 1, if even 2^0 = 0. So 
+                B = B - B/3;//If odd, remainder is 1, and subtracts it. Else does nothing
+                G = G - G/3;
+                 */
+                putc(R,fq);
+                putc(G,fq);
                 
-                printf("\n\nR: %d G: %d B:%d ",R,G,B);
+                putc(B,fq);
+              //  printf("Image %d,%d : R:%d G:%d B:%d   %d  \n",i,j,image[(i*h+j)][0],image[(i*h+j)][1],image[(i*h+j)][2],i*h+j);
+                
+              //  printf("%d, %d R: %d G: %d B:%d\n\n",i,j,R,G,B);
             }
+            //No need for this because I am now saving padding data as well
+            
+            //putc(0,fq);//For 4 bytes padding
+            //putc(0,fq);
+            
+            
+            
+            
+            
         }
-
-
+        putc(0,fq);//For 4 bytes padding
+        putc(0,fq);
+       
         
 
    //END TEST
 
     fclose(fp);
+    fclose(fq);
 } 
 
 
